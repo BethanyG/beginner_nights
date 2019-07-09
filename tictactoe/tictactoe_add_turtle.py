c152c0b2dd4d9ca81efcd2c1f598d37b7c084209
Bethany Garcia <bethanymgarcia@gmail.com>
2019-07-09 12:07:36 -0700

    Cleaned up code comments and unified colors for board and players.  Added __main__().

--- tictactoe/tictactoe_add_turtle.py
+++ tictactoe/tictactoe_add_turtle.py
@@ -1,7 +1,6 @@
 #!/bin/python3
 
 import turtle
-import math
 import random
 
 
@@ -66,12 +65,17 @@ def draw_splash_screen():
     taylor.goto(-170, 200)
     taylor.setheading(0)
     #'#CE0C24' dk red
-    taylor.color('#f08504')
-    taylor.write('Welcome to Tic Tac Toe!', font=("Fredericka the Great", 30, "bold"))
+    # taylor.color('#000000') #black
+    taylor.color('#4269f5')  # dk_blue
+    # taylor.color('#f08504') #goldenrod
+    taylor.write('Welcome to Tic Tac Toe!',
+                 font=("Fredericka the Great", 30, "bold"))
 
     taylor.pendown()
     taylor.goto(-150, 150)
+    taylor.color('#000000') #this avoids a weird bug where drawing glitches
     taylor.pencolor('#4269f5') #dk_blue
+    # taylor.color('#a442f5') #lavendar
     taylor.forward(300)
     taylor.right(90)
     taylor.forward(150)
@@ -88,8 +92,9 @@ def draw_splash_screen():
     taylor.penup()
     taylor.forward(35)
     taylor.pendown()
-    taylor.pensize(8)
-    taylor.color('#f08504')
+    taylor.pensize(12)
+    taylor.color('#a442f5')  # lavendar
+    # taylor.color('#f08504') # goldenrod
     taylor.forward(135)
     taylor.penup()
     taylor.goto(0, 150)
@@ -99,6 +104,7 @@ def draw_splash_screen():
     taylor.pendown()
     taylor.forward(135)
     taylor.penup()
+    taylor.color('#f08504')  # goldenrod
     taylor.goto(-75, 15)
     taylor.setheading(0)
     taylor.pendown()
@@ -109,6 +115,7 @@ def draw_splash_screen():
     taylor.color('#4269f5')
     taylor.write('Please click X or O to play', font=("Handlee", 24, "bold"))
 
+    #listener
     window.onclick(player_choose_letter, add=True)
 
 
@@ -116,10 +123,10 @@ def draw_board():
     global game_is_playing, game_board
 
     taylor.goto(-150, 150)
-    taylor.dot(12)
 
     # border
-    taylor.color('#f08504')  # goldenrod color
+    taylor.color('#4269f5')  # dk_blue
+    #taylor.color('#f08504')  # goldenrod color
     taylor.pendown()
 
     for number in range(4):
@@ -129,7 +136,7 @@ def draw_board():
     taylor.penup()
 
     # Set a New Color for the Coordinate Lines
-    taylor.color('#8d024a')  # burgundy
+    #taylor.color('#8d024a')  # burgundy
 
     # Cell dividing Lines
     taylor.setheading(270)
@@ -169,7 +176,8 @@ def draw_play_again():
     taylor.penup()
     taylor.goto(0, 50)
     taylor.pendown()
-    taylor.color('#000000')
+    taylor.color('#4269f5')  # dk_blue
+    # taylor.color('#000000')
     taylor.write('Play Again??', font=("Fredericka the Great", 40, "bold"), align="center")
 
     #No arrow (dk red, down)
@@ -185,11 +193,16 @@ def draw_play_again():
     taylor.right(120)
     taylor.forward(50)
     taylor.end_fill()
+    taylor.penup()
+    taylor.goto(-50, -60)
+    taylor.pendown()
+    taylor.write('No', font=("Fredericka the Great", 30, "bold"), align="center")
 
     #YES arrow (green, up)
     taylor.setheading(0)
     taylor.penup()
-    taylor.forward(60)
+    taylor.goto(40, 40)
+    #taylor.forward(60)
     taylor.right(60)
     taylor.pendown()
     taylor.begin_fill()
@@ -200,6 +213,10 @@ def draw_play_again():
     taylor.right(120)
     taylor.forward(50)
     taylor.end_fill()
+    taylor.penup()
+    taylor.goto(50, -60)
+    taylor.pendown()
+    taylor.write('YES', font=("Fredericka the Great", 30, "bold"), align="center")
 
     window.onclick(handle_play_again, add=True)
 
@@ -209,18 +226,20 @@ def handle_play_again(x, y):
     taylor.penup()
     taylor.goto(x, y)
 
-    # the first element is the player, the second is the computer.
-    if ((-59 < x < -29) and (-3 < y < 43)):
+    # Player answers NO
+    if ((-100 < x < -1) and (-120 < y < 45)):
         window.clearscreen()
         taylor.goto(-170, 200)
         taylor.setheading(0)
-        taylor.color('#f08504')
+        taylor.color('#4269f5')  # dk_blue
+        # taylor.color('#f08504') #goldenrod
         taylor.write('Thank you for Playing!!', font=("Fredericka the Great", 40, "bold"), align="center")
         game_is_playing = False
-        window.ontimer(window.bye, 2000)
+        window.ontimer(window.bye, 1000)
 
-    elif ((30 <x < 58) and (-3 < y < 43)):
-        window.ontimer(draw_splash_screen, 3000)
+    # Player answers YES
+    if ((0 <x < 100) and (-120 < y < 45)):
+        window.ontimer(draw_splash_screen, 1000)
 
 
 def player_choose_letter(x, y):
@@ -237,27 +256,33 @@ def player_choose_letter(x, y):
     O_first = ('O', 'X')
     X_first = ('X', 'O')
 
-    # the first element is the player, the second is the computer.
+    # Player chooses O
     if ((-150 < x < -1) and (1 < y < 150)):
         window.clearscreen()
         taylor.penup()
         taylor.goto(0,0)
-        taylor.color('#000000')
-        taylor.write(f'Player chooses {O_first[0]}!', font=("Fredericka the Great", 40, "bold"), align="center")
+        taylor.color('#4269f5')  # dk_blue
+        # taylor.color('#000000')
+        taylor.write(f'Player chooses {O_first[0]}',
+                     font=("Fredericka the Great", 40, "bold"), align="center")
         taylor.goto(0, -40)
-        taylor.write(f'Computer will be {O_first[1]}!', font=("Fredericka the Great", 40, "bold"), align="center")
+        taylor.write(f'Computer will be {O_first[1]}',
+                     font=("Fredericka the Great", 40, "bold"), align="center")
 
         players = O_first
 
-
-    elif ((1 <x < 150) and (2 < y < 150)):
+    # Player chooses X
+    elif ((1 <x < 150) and (1 < y < 150)):
         window.clearscreen()
         taylor.penup()
         taylor.goto(0, 20)
-        taylor.color('#000000')
-        taylor.write(f'Player chooses {X_first[0]}!', font=("Fredericka the Great", 40, "bold"), align="center")
+        taylor.color('#4269f5')  # dk_blue
+        # taylor.color('#000000')
+        taylor.write(f'Player chooses {X_first[0]}',
+                     font=("Fredericka the Great", 40, "bold"), align="center")
         taylor.goto(0, -40)
-        taylor.write(f'Computer will be {X_first[1]}!', font=("Fredericka the Great", 40, "bold"), align="center")
+        taylor.write(f'Computer will be {X_first[1]}',
+                     font=("Fredericka the Great", 40, "bold"), align="center")
 
         players = X_first
 
@@ -267,24 +292,23 @@ def player_choose_letter(x, y):
 
 def set_first_move():
     #import globals so they can be updated
-    global players, first_player, turn, game_is_playing
+    global players, first_player, turn
 
     #Randomly choose the player who goes first.
     first_player = 'Computer' if random.randint(0, 1) == 1 else 'Player'
 
     #Set the player letter for the turn
-    if first_player == 'Computer':
-        turn = players[1]
-    else:
-        turn = players[0]
+    turn = players[1] if first_player == 'Computer' else players[0]
+
 
     #Print first_player to screen
     taylor.penup()
     taylor.goto(0, 170)
-    taylor.color('#000000')
-    taylor.write(f'{first_player} goes first.', font=("Fredericka the Great", 40, "bold"), align="center")
+    taylor.color('#4269f5')  # dk_blue
+    # taylor.color('#000000')
+    taylor.write(f'{first_player} goes first.',
+                 font=("Fredericka the Great", 40, "bold"), align="center")
 
-    #game_is_playing = True
     print('FROM SET PLAYER::  ', turn)
 
     draw_board()
@@ -293,6 +317,9 @@ def set_first_move():
 
 
 def draw_x(x1, y1, x2, y2):
+    taylor.color('#a442f5')  # lavendar
+    taylor.pensize(7)
+
     #left diagonal
     taylor.penup()
     taylor.goto(x1, y1)
@@ -315,6 +342,9 @@ def draw_x(x1, y1, x2, y2):
 
 
 def draw_o(x, y):
+
+    taylor.color('#f08504')  # goldenrod
+    taylor.pensize(7)
     taylor.penup()
     taylor.goto(x, y)
     taylor.setheading(0)
@@ -326,81 +356,61 @@ def draw_o(x, y):
 def play_square(x, y):
     global game_board, turn, O_moves, X_moves
 
-    print(x, y)
+    print(x, y) #log clicked area to test that it's working
 
+    #Square 1 - upper left
     if ((-150 < x < -50) and (50 < y < 150)):
         draw_x(*X_moves[1]) if turn == 'X' else draw_o(*O_moves[1])
         game_board[1] = turn
-        if turn == players[1]:
-            turn = players[0]
-        else:
-            turn = players[1]
+        turn = players[0] if turn == players[1] else players[1]
 
+    #Square 2 - upper center
     if ((-51 < x < 50) and (50 < y < 150)):
         draw_x(*X_moves[2]) if turn == 'X' else draw_o(*O_moves[2])
         game_board[2] = turn
-        if turn == players[1]:
-            turn = players[0]
-        else:
-            turn = players[1]
+        turn = players[0] if turn == players[1] else players[1]
 
+    #Square 3 - upper right
     if ((51 < x < 150) and (50 < y < 150)):
         draw_x(*X_moves[3]) if turn == 'X' else draw_o(*O_moves[3])
         game_board[3] = turn
+        turn = players[0] if turn == players[1] else players[1]
 
-        if turn == players[1]:
-            turn = players[0]
-        else:
-            turn = players[1]
-
+    # Square 4 - center left
     if ((-150 < x < -50) and (-50 < y < 51)):
         draw_x(*X_moves[4]) if turn == 'X' else draw_o(*O_moves[4])
         game_board[4] = turn
+        turn = players[0] if turn == players[1] else players[1]
 
-        if turn == players[1]:
-            turn = players[0]
-        else:
-            turn = players[1]
-
+    # Square 5 - center
     if ((-51 < x < 50) and (-50 < y < 51)):
         draw_x(*X_moves[5]) if turn == 'X' else draw_o(*O_moves[5])
         game_board[5] = turn
-        if turn == players[1]:
-            turn = players[0]
-        else:
-            turn = players[1]
+        turn = players[0] if turn == players[1] else players[1]
 
+    # Square 6 - center right
     if ((51 < x < 150) and (-50 < y < 51)):
         draw_x(*X_moves[6]) if turn == 'X' else draw_o(*O_moves[6])
         game_board[6] = turn
-        if turn == players[1]:
-            turn = players[0]
-        else:
-            turn = players[1]
+        turn = players[0] if turn == players[1] else players[1]
 
+    # Square 7 - lower left
     if ((-150 < x < -50) and (-150 < y < -51)):
         draw_x(*X_moves[7]) if turn == 'X' else draw_o(*O_moves[7])
         game_board[7] = turn
-        if turn == players[1]:
-            turn = players[0]
-        else:
-            turn = players[1]
+        turn = players[0] if turn == players[1] else players[1]
 
+    # Square 8 - lower center
     if ((-51 < x < 50) and (-150 < y < -51)):
         draw_x(*X_moves[8]) if turn == 'X' else draw_o(*O_moves[8])
         game_board[8] = turn
-        if turn == players[1]:
-            turn = players[0]
-        else:
-            turn = players[1]
+        turn = players[0] if turn == players[1] else players[1]
 
+    # Square 9 - lower right
     if ((51 < x < 150) and (-150 < y < -51)):
         draw_x(*X_moves[9]) if turn == 'X' else draw_o(*O_moves[9])
         game_board[9] = turn
-        if turn == players[1]:
-            turn = players[0]
-        else:
-            turn = players[1]
+        turn = players[0] if turn == players[1] else players[1]
 
     print('CURRENT GAME BOARD::  ', game_board)
     print('NEXT TURN::  ', turn)
@@ -413,7 +423,8 @@ def space_taken():
     taylor.penup()
     taylor.goto(150, -160)
     taylor.pencolor('#000000')
-    taylor.write(f'That square\'s been taken already by {turn}', font=("Fredericka the Great", 20, "bold"), align="center")
+    taylor.write(f'That square\'s been taken already by {turn}',
+                 font=("Fredericka the Great", 20, "bold"), align="center")
 
     window.ontimer(taylor.clear, 1000)
 
@@ -427,9 +438,9 @@ def is_board_full():
 
 
 def is_winner(board, player):
-    # returns True if player has won.
+    # returns True if there is a win on the board.
 
-    letters = [f'{player}', f'{player}', f'{player}']
+    letters = [player, player, player]
 
     return (
         # across the bottom: 7, 8, & 9
@@ -479,7 +490,7 @@ def get_computer_move():
             if is_winner(copy, players[1]):
                 return number
 
-    # Then check if *player* can win on their next move & attempt a block.
+    # Then check if *player* can win on *their* next move & attempt a block.
     for number in range(1, 10):
         copy = game_board[:]
         if game_board[number] == '':
@@ -503,9 +514,10 @@ def get_computer_move():
 def play_game():
     global game_is_playing
 
+    #set the listener for the current game
     window.onclick(play_square)
 
-    #Main loop for current game
+    #Main event loop for current game
     while game_is_playing:
 
         #Players turn
@@ -519,19 +531,24 @@ def play_game():
                 window.clearscreen()
                 taylor.goto(0, 0)
                 taylor.color('#f08504')
-                taylor.write("You've beaten the Computer.  YAY!!", font=("Fredericka the Great", 40, "bold"),
+                taylor.write("You've beaten the Computer.  YAY!!",
+                             font=("Fredericka the Great", 40, "bold"),
                              align="center")
+
                 game_is_playing = False
                 window.ontimer(draw_play_again, 2000)
                 break
+
             else:
                 #Check for tie
                 if is_board_full():
                         window.clearscreen()
                         taylor.goto(0, 0)
                         taylor.color('#f08504')
-                        taylor.write('The game is a tie!', font=("Fredericka the Great", 40, "bold"),
+                        taylor.write('The game is a tie!',
+                                     font=("Fredericka the Great", 40, "bold"),
                                      align="center")
+
                         game_is_playing = False
                         window.ontimer(draw_play_again, 2000)
                         break
@@ -539,6 +556,8 @@ def play_game():
         #Computers turn
         if turn == players[1]:
             move = get_computer_move()
+
+            #use O coords as a proxy for a square click
             play_square(*O_moves[move])
 
             #Force screen update so that variables are in sync
@@ -552,6 +571,7 @@ def play_game():
                 taylor.write('The computer has beaten you! \nYou lose. Sad Times.',
                              font=("Fredericka the Great", 40, "bold"),
                              align="center")
+
                 game_is_playing = False
                 window.ontimer(draw_play_again, 2000)
                 break
@@ -562,14 +582,18 @@ def play_game():
                     window.clearscreen()
                     taylor.goto(0, 0)
                     taylor.color('#f08504')
-                    taylor.write('The game is a tie!', font=("Fredericka the Great", 40, "bold"),
+                    taylor.write('The game is a tie!',
+                                 font=("Fredericka the Great", 40, "bold"),
                                  align="center")
+
                     game_is_playing = False
                     window.ontimer(draw_play_again, 2000)
                     break
 
 
-draw_splash_screen()
+# Entry point.
+if __name__ == "__main__":
+    draw_splash_screen()
 
-#Start Turtle/Tk Loop
-window.mainloop()
+    #Start Turtle/Tk Loop
+    window.mainloop()
